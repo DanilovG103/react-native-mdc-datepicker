29cd554b661dd5e482837df613adfac556570b97
DanilovG103 <danilov2706@gmail.com>
2023-10-12 14:48:04 +0400

    feat: update example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,53 +1,59 @@
 import React, { useCallback, useState } from 'react';
 
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import { MDCDatePicker, MDCTimePicker } from 'react-native-mdc-datepicker';
+import {
+  DatePicker,
+  TimePicker,
+  type TimePickerResult,
+} from 'react-native-mdc-datepicker';
 
 const minDate = new Date(2020, 5, 14);
 const maxDate = new Date(2023, 11, 24);
 
 export default function App() {
+  const [time, setTime] = useState<TimePickerResult | null>(null);
+  const [timeVisible, setTimeVisible] = useState(false);
+  const [dateVisible, setDateVisible] = useState(false);
+  const [rangeVisible, setRangeVisible] = useState(false);
+
   const [selected, setSelected] = useState<Date | null>(null);
   const [selectedRange, setSelectedRange] = useState<{
     start: Date | null;
     end: Date | null;
   }>({ start: null, end: null });
-  const presentDefault = useCallback(async () => {
-    try {
-      const date = await MDCDatePicker.present({
-        minDate,
-        maxDate,
-        value: selected,
-        dynamicColors: true,
-      });
 
-      setSelected(date);
-    } catch (err) {
-      console.log(err);
-    }
-  }, [selected]);
+  const presentDefault = useCallback(async () => {
+    setDateVisible(true);
+    // try {
+    //   const date = await MDCDatePicker.present({
+    //     minDate,
+    //     maxDate,
+    //     value: selected,
+    //     dynamicColors: true,
+    //   });
+
+    //   setSelected(date);
+    // } catch (err) {
+    //   console.log(err);
+    // }
+  }, []);
 
   const presentRange = useCallback(async () => {
-    try {
-      const val = await MDCDatePicker.presentRange({
-        minDate,
-        maxDate,
-        dynamicColors: true,
-        ...selectedRange,
-      });
-      console.log(val);
-      setSelectedRange(val);
-    } catch {}
-  }, [selectedRange]);
+    setRangeVisible(true);
+    // try {
+    //   const val = await MDCDatePicker.presentRange({
+    //     minDate,
+    //     maxDate,
+    //     dynamicColors: true,
+    //     ...selectedRange,
+    //   });
+    //   console.log(val);
+    //   setSelectedRange(val);
+    // } catch {}
+  }, []);
 
   const presentTimePicker = useCallback(async () => {
-    try {
-      const time = await MDCTimePicker.present({
-        format: '24',
-        mode: 'clock',
-      });
-      console.log(time);
-    } catch {}
+    setTimeVisible(true);
   }, []);
 
   return (
@@ -58,9 +64,40 @@ export default function App() {
       <TouchableOpacity onPress={presentRange}>
         <Text>Show Date Range Picker</Text>
       </TouchableOpacity>
+      {time && (
+        <Text>
+          Time is {time.hour}:{time.minute}
+        </Text>
+      )}
       <TouchableOpacity onPress={presentTimePicker}>
         <Text>Show Time Picker</Text>
       </TouchableOpacity>
+      <TimePicker
+        visible={timeVisible}
+        setVisible={setTimeVisible}
+        format="24"
+        mode="clock"
+        onSelect={setTime}
+      />
+      <DatePicker
+        visible={dateVisible}
+        setVisible={setDateVisible}
+        value={selected}
+        minDate={minDate}
+        dynamicColors
+        maxDate={maxDate}
+        onSelect={setSelected}
+      />
+      <DatePicker
+        dynamicColors
+        mode="range"
+        visible={rangeVisible}
+        setVisible={setRangeVisible}
+        minDate={minDate}
+        maxDate={maxDate}
+        onSelect={setSelectedRange}
+        {...selectedRange}
+      />
     </View>
   );
 }
